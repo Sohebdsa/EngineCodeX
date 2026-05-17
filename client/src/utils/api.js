75c237f4ffd1573d437c12d616dafe1b52e186ba
sohebdsa <sohebhashmidsa@gmail.com>
2026-05-17 19:06:08 +0530

    refactor: use dynamic BASE_URL for API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://65.2.124.4:3001';
+const BASE_URL = import.meta.env.VITE_API_URL || (import.meta.env.PROD ? '' : 'http://localhost:3001');
 
 
 async function request(method, url, body) {
